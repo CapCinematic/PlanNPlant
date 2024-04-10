2493012e6af8b1e4ad83567b16acc5773042fac7
Isaiah Richardson <capcinematic@gmail.com>
2024-04-10 14:37:40 -0700

    add Banner component and CSS

---
+++ plan-n-plant-app/src/Banner.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h1>Welcome to Plan N' Plant!</h1>
+      <div className="home-button">
+        <Link to="/">
+            <span>⌂</span>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
